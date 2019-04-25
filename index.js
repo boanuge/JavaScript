fe12d60b002643945ae3e1bd0e82bb27bb2fbc1a
The Bible-smith <boanuge@users.noreply.github.com>
2019-04-25 10:38:32 +0900

    Update index.js
    #npm init
    #npm install
    #npm install express --save
    #npm install serve-index --save
    #npm install body-parser --save
    #node index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var bodyParser = require("body-parser");
 
 var app = express();
 var port = 3000;
-var path = __dirname + "/";
 
 // POST 데이터 처리
 // ?param=value&param=value 처리를 위해
@@ -12,22 +11,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 전달된 데이터를 JSON 형식으로 파싱
 app.use(bodyParser.json());
 
+// 미들웨어 타임스탬프
+var requestTime = function (req, res, next) {
+    req.requestTime = new Date().toString();
+    next();
+}
+app.use(requestTime);
+
 // 디렉토리 목록 출력
-app.use("/", express.static(path), serveIndex(path, {
-	icons: true,
-	view: "details"
-}));
+var path = __dirname + "/static";
+
+app.use("/public", express.static(path)
+    , serveIndex(path, { icons: true, view: "details" })
+);
+
+// 각각의 패스 라우팅
+app.get(/(create|list|done)/, function (req, res) {
+    res.send(req.originalUrl + " 호출됨");
+    console.log(req.originalUrl + " is called.");
+});
+
+// 패스를 통해 값 전달
+app.get("/user/:userID/book/:bookID", function (req, res) {
+    res.send(req.params);
+    console.log("URL = " + req.originalUrl);
+    console.log(req.params);
+    console.log(req.query);
+});
 
 // 서버 포트 리스닝
-var server = app.listen(port, function() {
-	console.log("Server is listening on port "+ port +".");
+var server = app.listen(port, function () {
+    console.log("Server is listening on port " + port + ".");
 });
 
-process.on("SIGINT", function() {
-	server.close(function () {
-		console.log("Server is closed.");
+process.on("SIGINT", function () {
+    server.close(function () {
+        console.log("Server is closed.");
 
-		// Close db connections
-		db.close();
-	});
+        // Close db connections
+        //db.close();
+    });
 });
+
+// SQLite3 database
+/*
+console.log("//SQLite3 DB");
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("./test_db.db");
+db.run("insert into test_db values (?, ?, ?)", ["1", "2", "3"], function () {
+    console.log("테스트");
+})
+db.each("select * from test_db", function (err, row) {
+    console.log(row);
+});
+db.all("select * from test_db", function (err, rows) {
+    console.log(rows);
+});
+db.close();
+*/
